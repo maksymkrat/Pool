a97a506eceadfc5409b28e9ab5f4bd3fa8baad5f
mkrat <mkrat@mint-innovations.com>
2022-11-04 11:17:17 +0200

    add word test

--- Pool.Client/Pages/Words-test.razor.cs
+++ Pool.Client/Pages/Words-test.razor.cs
@@ -1,8 +1,46 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Pool.Client.Services;
+using Pool.Shared.Models;
 
 namespace Pool.Client.Pages;
 
 public class Words_test_razor : ComponentBase
 {
+    [Inject] private WordService _wordService { get; set; }
+    protected Word MainWord { get; set; }
+    protected List<Word> Words{ get; set; }
+    protected Random rnd = new Random();
+    protected string resultStyle = "light";
+    protected string mainWordStyle = "light";
+    protected int rndWord = 0;
+    private readonly Guid userId = new Guid("23a2dcb7-38b5-44b4-85e9-9e6af7f4646f");
+
+
+    protected async override Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        Words = await _wordService.GetFourRandomWords(userId);
+        MainWord = Words.ElementAt(rnd.Next(0, Words.Count));
+    }
+
+    protected override void OnInitialized()
+    {
+          
+        
+    }
     
+
+    protected void ChangeStyleMainWord(string style)
+    {
+        mainWordStyle = style;
+        
+    }
+
+    protected async void ResetWord()
+    {
+        Words = await _wordService.GetFourRandomWords(userId);
+        MainWord = Words.ElementAt(rnd.Next(0, Words.Count));
+        resultStyle = "light";
+        mainWordStyle = resultStyle;
+    }
 }
