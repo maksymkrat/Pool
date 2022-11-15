2b4f0fa4471d2051e992d4fb586f2894ef1e8c8b
mkrat <mkrat@mint-innovations.com>
2022-11-15 13:47:47 +0200

    refactor

--- Pool.Shared/Models/LoginRequest.cs
+++ Pool.Shared/Models/LoginRequest.cs
@@ -4,12 +4,12 @@ namespace Pool.Shared.Models;
 
 public class LoginRequest
 {
-    // [Required(ErrorMessage = "enter email")]
-    // [StringLength(100, ErrorMessage = "min 4 symbols", MinimumLength = 4)]
+    [Required(ErrorMessage = "enter email")]
+    [StringLength(100, ErrorMessage = "min 4 symbols", MinimumLength = 4)]
     public string UserName { get; set; }
     
-    // [Required(ErrorMessage = "enter password")]
-    // [StringLength(100, ErrorMessage = "min 4 symbols", MinimumLength = 4)]
-    // [DataType(DataType.Password)]
+    [Required(ErrorMessage = "enter password")]
+    [StringLength(100, ErrorMessage = "min 4 symbols", MinimumLength = 4)]
+    [DataType(DataType.Password)]
     public string Password { get; set; }    
 }
