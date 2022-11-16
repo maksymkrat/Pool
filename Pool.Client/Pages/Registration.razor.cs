e33ce447b8574b3e7e025ddf41359561fbfdc48a
mkrat <mkrat@mint-innovations.com>
2022-11-16 09:45:22 +0200

    add register validation

--- Pool.Client/Pages/Registration.razor.cs
+++ Pool.Client/Pages/Registration.razor.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Pool.Shared.Models;
 
 namespace Pool.Client.Pages;
 
 public class Registration_razor : ComponentBase
 {
-    
+    protected RegistrationModel newUser = new RegistrationModel();
+
+    protected void HandleValidationSubmit()
+    {
+        
+    }
 }
--- Pool.Shared/Models/RegistrationModel.cs
+++ Pool.Shared/Models/RegistrationModel.cs
@@ -1,12 +1,32 @@
-﻿namespace Pool.Shared.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pool.Shared.Models;
 
 public class RegistrationModel
 {
+    [Required(ErrorMessage = "FirstName email")]
     public string FirstName { get; set; }
+    
+    [Required(ErrorMessage = "LastName email")]
     public string LastName { get; set; }
+    
+    [Required(ErrorMessage = "Username email")]
     public string Username { get; set; }
+    
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
     public string Email { get; set; }
+    
+    [Phone]
     public string Phone { get; set; }
+    
+    [Required(ErrorMessage = "Enter password")]
+    [StringLength(100, ErrorMessage = "min 4 symbols", MinimumLength = 4)]
+    [DataType(DataType.Password)]
     public string Password { get; set; }
+    
+    [Required(ErrorMessage = "Confirm Password is required")]
+    [StringLength(100, ErrorMessage = "Must be between 4 and 100 characters", MinimumLength = 4)]
+    [DataType(DataType.Password)]
+    [Compare("Password")]
     public string PasswordConfirm { get; set; }
 }
