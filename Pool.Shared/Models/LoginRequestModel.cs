91207d451574426270c6c578706e9c9f79fc4bb6
mkrat <mkrat@mint-innovations.com>
2022-11-29 10:11:40 +0200

    change login page and add validation login fields

--- Pool.Shared/Models/LoginRequestModel.cs
+++ Pool.Shared/Models/LoginRequestModel.cs
@@ -4,12 +4,13 @@ namespace Pool.Shared.Models;
 
 public class LoginRequestModel
 {
-    [Required(ErrorMessage = "enter email")]
+    [Required(ErrorMessage = "Enter email")]
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
     [StringLength(100, ErrorMessage = "min 4 symbols", MinimumLength = 4)]
     public string UserName { get; set; }
-    
-    [Required(ErrorMessage = "enter password")]
+        
+    [Required(ErrorMessage = "Enter password")]
     [StringLength(100, ErrorMessage = "min 4 symbols", MinimumLength = 4)]
     [DataType(DataType.Password)]
-    public string Password { get; set; }    
+    public string Password { get; set; }
 }
